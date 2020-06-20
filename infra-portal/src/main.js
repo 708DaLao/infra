d6626a4f30f1f4edcfed13594d9619b9fc325297
dalao708 <1376459917@qq.com>
2020-06-20 18:09:31 +0800

    重新初始化infra-portal

--- infra-portal/src/main.js
+++ infra-portal/src/main.js
@@ -1,21 +1,12 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import 'normalize.css/normalize.css' //初始化重置css样式
-import 'font-awesome/css/font-awesome.min.css' //使用font-awesome字体图标库
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-Vue.use(ElementUI)
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  store,
+  render: h => h(App)
+}).$mount("#app");
