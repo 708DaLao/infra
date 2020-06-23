465a0d893c2767603455b19e827b6b6a029e18cd
曾 <1376459917@qq.com>
2020-06-23 17:58:59 +0800

    瞎搞了一些东西

--- infra-portal/src/utils/request.js
+++ infra-portal/src/utils/request.js
@@ -9,16 +9,11 @@ const http = axios.create({
     timeout: 5000 // request timeout
 })
 
-// http请求
+// request拦截器
 http.interceptors.request.use(
     config => {
-        // do something before request is sent
-
         if (store.getters.token) {
-            // let each request carry token
-            // ['X-Token'] is a custom headers key
-            // please modify it according to the actual situation
-            config.headers['infra-Token'] = getToken()
+            config.headers['Infra-Token'] = getToken() // 每个请求都携带token
         }
         return config
     },
@@ -29,24 +24,14 @@ http.interceptors.request.use(
     }
 )
 
-// http响应
+// response拦截器
 http.interceptors.response.use(
-    /**
-     * If you want to get http information such as headers or status
-     * Please return  response => response
-     */
-
-    /**
-     * Determine the request status by custom code
-     * Here is just an example
-     * You can also judge the status by HTTP Status Code
-     */
     response => {
         const res = response.data
 
         // if the custom code is not 20000, it is judged as an error.
         if (res.code !== 200) {
-            console.log("..........")
+            console.log(JSON.stringify(res))
             return Promise.reject(new Error(res.message || 'Error'))
         } else {
             return res
