0831bd39cb3fb8c8ff077819fb1d3f95cda504bc
zzd <1376459917@qq.com>
2020-08-27 09:38:47 +0800

    404页面

--- infra-portal/src/layout/components/Sidebar/index.vue
+++ infra-portal/src/layout/components/Sidebar/index.vue
@@ -1,44 +1,47 @@
 <template>
-    <div>
-        <div class="logo">
-            <img v-if="!$store.getters.isCollapse" src="../../../assets/img/logo.png" alt=""
-                 style="height: 100%;width: 200px">
-            <img v-else src="../../../assets/img/logo-1.png" alt="" style="height: 100%;width: 65px">
-        </div>
-        <el-collapse-transition>
-            <el-menu
-                :background-color="baseBgColor"
-                :collapse="$store.getters.isCollapse"
-            >
-                <sidebar-item v-for="route in routes" :key="route.path" :item="route" :base-path="route.path"></sidebar-item>
-            </el-menu>
-        </el-collapse-transition>
+  <div>
+    <div class="logo">
+      <img v-if="!$store.getters.isCollapse" src="../../../assets/img/logo.png" alt="" style="height: 100%;width: 200px"/>
+      <img v-else src="../../../assets/img/logo-1.png" alt="" style="height: 100%;width: 65px"/>
     </div>
+    <el-collapse-transition>
+      <el-menu
+        :background-color="baseBgColor"
+        :collapse="$store.getters.isCollapse"
+      >
+        <sidebar-item
+          v-for="route in routes"
+          :key="route.path"
+          :item="route"
+          :base-path="route.path"
+        ></sidebar-item>
+      </el-menu>
+    </el-collapse-transition>
+  </div>
 </template>
 
 <script>
-    import SidebarItem from "./SidebarItem";
-    import styles from  "@/styles/index.scss"
-    export default {
-        name: "Sidebar",
-        components: {SidebarItem},
-        data() {
-            return {
-                baseBgColor: styles.baseBgColor
-            }
-        },
-        computed: {
-            routes() {
-                return this.$router.options.routes
-            }
-        }
+import SidebarItem from "./SidebarItem";
+import styles from "@/styles/index.scss";
+export default {
+  name: "Sidebar",
+  components: { SidebarItem },
+  data() {
+    return {
+      baseBgColor: styles.baseBgColor
+    };
+  },
+  computed: {
+    routes() {
+      return this.$router.options.routes;
     }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-    .logo {
-        width: auto; /*高度固定，宽度变化*/
-        height: 60px;
-    }
-
+.logo {
+  width: auto; /*高度固定，宽度变化*/
+  height: 60px;
+}
 </style>
--- infra-portal/src/router/index.js
+++ infra-portal/src/router/index.js
@@ -6,7 +6,7 @@ import Login from "../views/login.vue";
 import NProgress from "nprogress";
 
 /* Layout 布局*/
-import Layout from '@/layout'
+import Layout from "@/layout";
 
 /**
  * 路由配置指南：
@@ -30,100 +30,110 @@ export const constantRoutes = [
   {
     path: "/",
     component: Layout,
-    children: [{
-      path: 'dashboard',
-      name: 'Dashboard',
-      component: () => import('views/dashboard/index'),
-      meta: { title: '控制台', icon: 'el-icon-help' },
-    }]
-  },
-  {
-    path: '/nested',
-    component: Layout,
-    redirect: '/nested/menu1',
-    name: 'Nested',
-    meta: {title: 'Nested', icon: 'el-icon-menu'},
+    redirect: "dashboard",
     children: [
       {
-        path: 'menu1',
-        component: () => import('@/views/nested/menu1/index'), // Parent router-view
-        name: 'Menu1',
-        meta: { title: 'Menu1' },
+        path: "dashboard",
+        name: "Dashboard",
+        component: () => import("views/dashboard/index"),
+        meta: { title: "控制台", icon: "el-icon-help" }
+      }
+    ]
+  },
+  {
+    path: "/nested",
+    component: Layout,
+    redirect: "/nested/menu1",
+    name: "Nested",
+    meta: { title: "Nested", icon: "el-icon-menu" },
+    children: [
+      {
+        path: "menu1",
+        component: () => import("@/views/nested/menu1/index"), // Parent router-view
+        name: "Menu1",
+        meta: { title: "Menu1" },
         children: [
           {
-            path: 'menu1-1',
-            component: () => import('@/views/nested/menu1/menu1-1'),
-            name: 'Menu1-1',
-            meta: { title: 'Menu1-1' }
+            path: "menu1-1",
+            component: () => import("@/views/nested/menu1/menu1-1"),
+            name: "Menu1-1",
+            meta: { title: "Menu1-1" }
           },
           {
-            path: 'menu1-2',
-            component: () => import('@/views/nested/menu1/menu1-2'),
-            name: 'Menu1-2',
-            meta: { title: 'Menu1-2' },
+            path: "menu1-2",
+            component: () => import("@/views/nested/menu1/menu1-2"),
+            name: "Menu1-2",
+            meta: { title: "Menu1-2" },
             children: [
               {
-                path: 'menu1-2-1',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-                name: 'Menu1-2-1',
-                meta: { title: 'Menu1-2-1' }
+                path: "menu1-2-1",
+                component: () =>
+                  import("@/views/nested/menu1/menu1-2/menu1-2-1"),
+                name: "Menu1-2-1",
+                meta: { title: "Menu1-2-1" }
               },
               {
-                path: 'menu1-2-2',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-                name: 'Menu1-2-2',
-                meta: { title: 'Menu1-2-2' }
+                path: "menu1-2-2",
+                component: () =>
+                  import("@/views/nested/menu1/menu1-2/menu1-2-2"),
+                name: "Menu1-2-2",
+                meta: { title: "Menu1-2-2" }
               }
             ]
           },
           {
-            path: 'menu1-3',
-            component: () => import('@/views/nested/menu1/menu1-3'),
-            name: 'Menu1-3',
-            meta: { title: 'Menu1-3' }
+            path: "menu1-3",
+            component: () => import("@/views/nested/menu1/menu1-3"),
+            name: "Menu1-3",
+            meta: { title: "Menu1-3" }
           }
         ]
       },
       {
-        path: 'menu2',
-        component: () => import('@/views/nested/menu2/index'),
-        meta: { title: 'menu2' }
+        path: "menu2",
+        component: () => import("@/views/nested/menu2/index"),
+        meta: { title: "menu2" }
       }
     ]
   },
   {
     path: "/login",
     component: Layout,
-    children: [{
-      path: 'login',
-      component: () => import('views/login'),
-      meta: { title: '登录页', icon: 'el-icon-help' },
-    }],
+    children: [
+      {
+        path: "login",
+        component: () => import("views/login"),
+        meta: { title: "登录页", icon: "el-icon-help" }
+      }
+    ]
   },
   {
     path: "/404",
     component: Layout,
-    children: [{
-      path: 'index',
-      name: 'index',
-      component: () => import('views/404/index'),
-      meta: { title: '404', icon: 'el-icon-warning-outline' }
-    }]
+    children: [
+      {
+        path: "index",
+        name: "index",
+        component: () => import("@/views/error/404/index"),
+        meta: { title: "404", icon: "el-icon-warning-outline" }
+      }
+    ]
   }
 ];
 
-const createRouter = () => new VueRouter({
-  // mode: 'history', // require service support
-  scrollBehavior: () => ({ y: 0 }),
-  routes: constantRoutes
-})
+const createRouter = () =>
+  new VueRouter({
+    // mode: 'history', // require service support
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes
+  });
 
-const router = createRouter()
+const router = createRouter();
 
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
-  const newRouter = createRouter()
-  router.matcher = newRouter.matcher // reset router
+  const newRouter = createRouter();
+  router.matcher = newRouter.matcher; // reset router
 }
 
-export default router
+export default router;
